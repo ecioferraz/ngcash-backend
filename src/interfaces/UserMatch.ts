5910eefdec8ecbb59a3f19185209efe3903621ae
Écio Ferraz <ecioferraz@hotmail.com>
2022-12-07 19:54:30 -0300

    refactor: interfaces

---
+++ src/interfaces/UserMatch.ts
@@ -0,0 +1,6 @@
+interface UserMatch {
+  id: string;
+  username: string;
+}
+
+export default UserMatch;
--- src/interfaces/UserWithoutPassword.ts
+++ src/interfaces/UserWithoutPassword.ts
@@ -1,7 +1,7 @@
+import UserMatch from './UserMatch';
+
 // interface created to deal with password deletion due to needing tsconfig's "strictNullChecks": true, because of zod's type inferring
-interface UserWithoutPassword {
-  id: string;
-  username: string;
+interface UserWithoutPassword extends UserMatch {
   password?: string;
   accountId: string;
 }
